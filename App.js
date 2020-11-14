0b0b697e87cff0f111903b1e6f58d9ce112d42eb
Douglas Franklin <douglasfkln@outlook.com>
2020-11-14 01:08:50 -0300

    ScrollView

--- App.js
+++ App.js
@@ -1,72 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      numero: 0,
-      botao: 'VAI',
-      ultimo: 0
+
     };
-
-    this.timer = null;
-    this.vai = this.vai.bind(this);
-    this.limpar = this.limpar.bind(this);
-  }
-
-  vai() {
-
-    if (this.timer != null) {
-      clearInterval(this.timer);
-      this.timer = null;
-      this.setState({ botao: "VAI" });
-    } else {
-      this.timer = setInterval(() => {
-        this.setState({
-          numero: this.state.numero + 0.1
-        });
-      }, 100);
-      this.setState({ botao: "PARAR" });
-    }
-  }
-
-  limpar() {
-    if (this.timer != null) {
-      clearInterval(this.timer);
-      this.timer = null;
-    }
-    this.setState({
-      ultimo: this.state.numero,
-      numero: 0,
-      botao: 'VAI'
-    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Image
-          source={require('./src/cronometro.png')}
-          style={styles.cronometro}
-        />
-        <Text style={styles.timer}>{this.state.numero.toFixed(1)}</Text>
 
-        <View style={styles.btnArea}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+        //  scrollEnabled={false}
+        >
+          <View style={[styles.box, styles.box1]}></View>
+          <View style={[styles.box, styles.box2]}></View>
+          <View style={[styles.box, styles.box3]}></View>
+          <View style={[styles.box, styles.box4]}></View>
+        </ScrollView>
 
-          <TouchableOpacity style={styles.btn} onPress={this.vai}>
-            <Text style={styles.btnTexto}>{this.state.botao}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.btn} onPress={this.limpar}>
-            <Text style={styles.btnTexto}>LIMPAR</Text>
-          </TouchableOpacity>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          horizontal={true}>
+          <View style={[styles.boxH, styles.box1]}></View>
+          <View style={[styles.boxH, styles.box2]}></View>
+          <View style={[styles.boxH, styles.box3]}></View>
+          <View style={[styles.boxH, styles.box4]}></View>
+        </ScrollView>
 
-        </View>
-
-        <View style={styles.areaUltimo}>
-          <Text style={styles.textoUltimo}>{this.state.ultimo > 0 ? 'Último tempo: ' + this.state.ultimo.toFixed(1) + 's' : ''}</Text>
-        </View>
       </View>
     );
   }
@@ -74,43 +40,26 @@ class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#00aeef'
+    flex: 1
   },
-  timer: {
-    marginTop: -160,
-    color: '#FFF',
-    fontSize: 65,
-    fontWeight: 'bold'
+  box: {
+    height: 250
   },
-  btnArea: {
-    marginTop: 70,
-    height: 40,
-    flexDirection: 'row',
+  boxH: {
+    height: 250,
+    width: 150
   },
-  btn: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFF',
-    height: 40,
-    margin: 17,
-    borderRadius: 9
+  box1: {
+    backgroundColor: 'red'
   },
-  btnTexto: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#00aeef'
+  box2: {
+    backgroundColor: 'green'
   },
-  areaUltimo: {
-    marginTop: 40
+  box3: {
+    backgroundColor: 'yellow'
   },
-  textoUltimo: {
-    fontStyle: 'italic',
-    fontSize: 25,
-    color: '#FFF'
+  box4: {
+    backgroundColor: 'blue'
   }
 });
 
---
+++ AppScrollView.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default App;
