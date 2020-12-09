b28f22d5fcc7f3d1ad7240a5bfdccd9d3faa24da
Douglas Franklin <douglasfkln@outlook.com>
2020-12-08 23:16:29 -0300

    AsyncStorage

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './AppDesafio1';
+import App from './AppAsyncStorage';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
